0a66bf026c59fdf9ac5a2279b5cb60584bcbd005
lglasser87 <lizbeth.cazarez@gmail.com>
2020-04-04 14:55:58 -0700

    fixed port, changed config.json to work with JawsDB for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var session = require("express-session");
 var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 4000;
+var PORT = process.env.PORT || 3306;
 var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
