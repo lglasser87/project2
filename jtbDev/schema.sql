9f91e93bcdd6d5d716597db36e1b029ac96fd405
Player 1 <j.banuelos00@gmail.com>
2020-04-11 11:54:31 -0700

    Updated server.js to allow for sequelilze. Initialized package.json.

---
+++ jtbDev/schema.sql
@@ -0,0 +1,4 @@
+-- Drops the textAdv if it exists currently --
+DROP DATABASE IF EXISTS textAdv;
+-- Creates the "textAdv" database --
+CREATE DATABASE textAdv;
--- jtbDev/server.js
+++ jtbDev/server.js
@@ -11,6 +11,9 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Requiring our models for syncing
+var db = require("./models");
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,8 +26,11 @@ app.use(express.static("app/public"));
 require("./app/routes/api-routes.js")(app);
 require("./app/routes/html-routes.js")(app);
 
-// Starts the server to begin listening
+// Syncing our sequelize models and then starting our Express app
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
 });
+
