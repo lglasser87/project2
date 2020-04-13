2ab06dd8220ce93a42d63366d23416bececcca08
Player 1 <j.banuelos00@gmail.com>
2020-04-12 17:07:08 -0700

    Added basic structure for the inspect command targeted at both the room and an item

---
+++ jtbDev/lib/commands/inspect.js
@@ -0,0 +1,14 @@
+let inspect =
+{
+    inspectRoom() {
+        return "Inspecting room";
+    },
+
+    inspectItem() {
+        return "inspecting item"
+    }
+    
+}
+
+console.log(inspect.inspectRoom());
+console.log(inspect.inspectItem());
